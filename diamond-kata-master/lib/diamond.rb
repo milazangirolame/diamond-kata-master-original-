fc32b583a77c5900c255dc2dc64d456eb4ca720b
Mila Zangirolame <milazangirolame@gmail.com>
2019-10-17 00:14:57 -0300

    building lines for the triangle mirror

--- diamond-kata-master/lib/diamond.rb
+++ diamond-kata-master/lib/diamond.rb
@@ -13,18 +13,16 @@ class Diamond
      if char == 'A'
        char + "\n"
      else
-
+     triangle
      end
-
   end
 
 
   def build_line(current_char, total, n)
-
-     if n == 0 #A
+    if n == 0 #A
       '_'*(total -1) + current_char  + '_'*(total -1) + "\n"
      else
-      puts "aqui caiu no else "
+    '_'*(total -1) + current_char + "_"*(n*2 - 1) + current_char + '_'*(total -1) + "\n"
     end
   end
 
