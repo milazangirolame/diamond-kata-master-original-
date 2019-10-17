c7463e6fe83c40e2e74e0aa31b9a7ebbe500ff77
Mila Zangirolame <milazangirolame@gmail.com>
2019-10-17 00:18:03 -0300

    building base and returning full answer

--- diamond-kata-master/lib/diamond.rb
+++ diamond-kata-master/lib/diamond.rb
@@ -13,17 +13,22 @@ class Diamond
      if char == 'A'
        char + "\n"
      else
-     triangle
+      triangle + build_base(char, a.length) + triangle.reverse! + "\n"
      end
   end
 
 
   def build_line(current_char, total, n)
-    if n == 0 #A
+    if n == 0
       '_'*(total -1) + current_char  + '_'*(total -1) + "\n"
      else
     '_'*(total -1) + current_char + "_"*(n*2 - 1) + current_char + '_'*(total -1) + "\n"
     end
+  end
+
+  def build_base(char, total)
+    i = total -1
+    char + '_'*(2*i-1) + char
   end
 
 end
